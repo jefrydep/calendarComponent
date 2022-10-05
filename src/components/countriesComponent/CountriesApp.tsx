185e8c64b074922d12ccd9c82a093f6061e80f23
jefry <jefrydep@gmail.com>
2022-10-05 13:13:44 -0500

    adding changes to json

--- src/components/countriesComponent/CountriesApp.tsx
+++ src/components/countriesComponent/CountriesApp.tsx
@@ -3,35 +3,62 @@ import provinces from "../../data/countries_peru.json";
 import { useEffect, useState } from "react";
 // const URL = "https://ubigeo-pe.herokuapp.com/v4?coddpto=21";
 export const CountriesApp = () => {
-  const [countries, setCountries] = useState([]);
-  const searchCties = () => {
-    
+  const [selectedDepartament, setselectedDepartament] = useState('')
+  const onSearchDepartments = (e: any) => {
+    console.log(e.target.value);
+    setselectedDepartament(e.target.value)
   };
-  const provincesPuno = provinces.filter(res => res.coddpto === '21' && res.codprov === '12')
-  const provinceSort= provincesPuno.sort();// setCountries()
-  const allProvinces = provinces.filter(res => res.tipo === 'provincia')
- console.log(allProvinces);
- 
-  useEffect(() => {
-    searchCties();
-  }, []);
-  
+  const allDepartments = provinces.filter(
+    (departaments) =>
+      departaments.coddist === "01" &&
+      departaments.codprov === "01" &&
+      departaments.tipo === "provincia"
+  );
 
+
+
+
+// const searchProvince = (provinceName :string)=>{
+
+//   const allProvinces = provinces.filter(
+//     (res) => res.tipo === "provincia" && res.nombre  === provinceName
+//   );
+//   return allProvinces;
+// }
+// console.log(searchProvince('PUNO'))
+
+const allProvinces = provinces.filter(
+  (res) =>res.coddpto ==='21' && res.tipo === "provincia"  
+);
+
+
+
+
+
+
+
+  console.log(allDepartments);
+
+  useEffect(() => {}, []);
 
   return (
-    < >
-    
-    <select name="provinces" id="">
-      {provinceSort.map(provinces => <option>{provinces.nombre}</option>)}
-    </select>
-    <br />
-    <select name="provinces" id="">
-      {allProvinces.map(res => <option>{res.nombre}</option>)}
-    </select>
-
-
-    </ >
+    <>
+      <h2>departamentos</h2>
+      
+      <select  onChange={onSearchDepartments}>
+        {allDepartments.map((deps) => (
+          <option>{deps.nombre}</option>
+        ))}
+      </select>
+      <br />
+      <br />
+      <hr />
+      <h2>provincias</h2>
+      <select name="provinces" id="">
+        {allProvinces.map((res) => (
+          <option>{res.nombre}</option>
+        ))}
+      </select>
+    </>
   );
 };
-
-
