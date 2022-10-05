ec7a64dca5c18b841d93cdddb79385640b664dd3
jefry <jefrydep@gmail.com>
2022-10-05 10:42:18 -0500

    filtering provinces

--- src/components/countriesComponent/CountriesApp.tsx
+++ src/components/countriesComponent/CountriesApp.tsx
@@ -1,6 +1,37 @@
-
+// import axios from "axios";
+import provinces from "../../data/countries_peru.json";
+import { useEffect, useState } from "react";
+// const URL = "https://ubigeo-pe.herokuapp.com/v4?coddpto=21";
 export const CountriesApp = () => {
+  const [countries, setCountries] = useState([]);
+  const searchCties = () => {
+    
+  };
+  const provincesPuno = provinces.filter(res => res.coddpto === '21' && res.codprov === '12')
+  const provinceSort= provincesPuno.sort();// setCountries()
+  const allProvinces = provinces.filter(res => res.tipo === 'provincia')
+ console.log(allProvinces);
+ 
+  useEffect(() => {
+    searchCties();
+  }, []);
+  
+
+
   return (
-    <div>CountriesApp</div>
-  )
-}
+    < >
+    
+    <select name="provinces" id="">
+      {provinceSort.map(provinces => <option>{provinces.nombre}</option>)}
+    </select>
+    <br />
+    <select name="provinces" id="">
+      {allProvinces.map(res => <option>{res.nombre}</option>)}
+    </select>
+
+
+    </ >
+  );
+};
+
+
